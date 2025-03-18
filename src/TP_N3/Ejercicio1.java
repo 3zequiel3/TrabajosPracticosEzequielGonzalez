b4c7ef15c986d3e8ee16c8f861fee59ee1d7b33a
Ezequiel <3zegonzalez@gmail.com>
2025-03-18 11:18:17 -0300

    Suba de carpetas

---
+++ src/TP_N3/Ejercicio1.java
@@ -0,0 +1,129 @@
+package TP_N3;
+
+import java.util.Scanner;
+
+class Estudiante {
+    private String nombre;
+    private String apellido;
+    private String curso;
+    private double calificacion;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String nombre, String apellido, String curso, double calificacion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.curso = curso;
+        this.calificacion = calificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Apellido del estudiante: "+apellido+"\n Nombre del estudiante: " + nombre+"\n Curso:"+curso+"\n Calificacion: "+calificacion);
+    }
+
+    public void subirCalificacion(double puntos){
+        if(this.calificacion+puntos<=10){
+            this.calificacion+=puntos;
+        }
+        else if (this.calificacion + puntos<=0){
+            this.calificacion = 0.0;
+        }else{
+            this.calificacion = 10.0;
+        }
+    }
+
+    public void bajarCalificacion(double puntos){
+        if(this.calificacion-puntos>=0 && this.calificacion+puntos<=10){
+            this.calificacion-=puntos;
+        }else{
+            this.calificacion = 0.0;
+        }
+    }
+
+}
+public class Ejercicio1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Estudiante estudiante1 = new Estudiante();
+
+        estudiante1.mostrarInfo();
+        System.out.println("""
+                Que deseas hacer?:
+                1.Mostrar Informacion
+                2.Subir Informacion
+                3.Bajar Calificacion
+                4.Subir Calificacion
+                """);
+        int opcion = scanner.nextInt();
+        switch(opcion){
+            case 1-> estudiante1.mostrarInfo();
+            case 2->{
+                System.out.print("\tDime el nombre del estudiante: ");
+                var nombre = scanner.nextLine();
+                System.out.println(" ");
+                System.out.print("\tDime el apellido del estudiante: ");
+                var apellido = scanner.nextLine();
+                System.out.println(" ");
+                System.out.print("\tDime el curso: ");
+                var curso = scanner.nextLine();
+                System.out.println(" ");
+                System.out.print("\tDime el calificacion: ");
+                var calificacion = scanner.nextDouble();
+                System.out.println(" ");
+                estudiante1.setNombre(nombre);
+                estudiante1.setApellido(apellido);
+                estudiante1.setCurso(curso);
+                estudiante1.setCalificacion(calificacion);
+                System.out.println(" ");
+            }
+            case 3->{
+                var puntos = scanner.nextDouble();
+                System.out.print("Cuantos puntos le bajaras al estudiante: ");
+                estudiante1.bajarCalificacion(puntos);
+                System.out.println(" ");
+            }
+            case 4->{
+                var puntos = scanner.nextDouble();
+                System.out.print("Cuantos puntos le subiras al estudiante: ");
+                estudiante1.subirCalificacion(puntos);
+                System.out.println(" ");
+            }
+            default -> System.out.println("Ingresa una opcion valida");
+        }
+
+    }
+
+}
