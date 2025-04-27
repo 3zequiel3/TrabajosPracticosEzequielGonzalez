72b35bdc7c85e5452959a217196b0c21ebec74ad
3zequiel3 <3zegonzalez@gmail.com>
2025-04-27 01:33:21 -0300

    Suba del TP6 (Colecciones)

---
+++ src/TP_N6/CategoriaProducto.java
@@ -0,0 +1,15 @@
+package TP_N6;
+
+public enum CategoriaProducto {
+    ALIMENTOS("Alimentos comestibles"),
+    ELECTRONICA("Dispositivos Electronicos"),
+    ROPA("Prendas para vestir"),
+    HOGAR("Articulos para el hogar");
+    private final String descripcion;
+    CategoriaProducto(String descripcion){
+        this.descripcion = descripcion;
+    }
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+}
